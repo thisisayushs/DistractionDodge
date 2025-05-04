a9ba41b02e8e881774f770d1e4b0401c5df381cf
Ayush Singh <singh.ayush.as@icloud.com>
2025-05-04 03:19:11 +0200

    Bug fixes

--- DistractionDodge/Home/Home.swift
+++ DistractionDodge/Home/Home.swift
@@ -11,8 +11,10 @@ struct Home: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+        
 }
 
 #Preview {
     Home()
+        .preferredColorScheme(.dark)
 }
